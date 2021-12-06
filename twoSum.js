45d7ba246eea8bdbe3c8f836bc01c3e27b9715c5
Blain Crawford <blaincrawford@Blains-MacBook-Pro.local>
2021-12-06 10:36:52 -0500

    Adding in first twoSum Algorith solution

--- twoSum.js
+++ twoSum.js
@@ -9,7 +9,71 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-    
+var twoSum = function (nums, target) {};
+
+/**
+ * Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ */
+
+//// Brute Force Method \\\\
+
+let twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
 };
 
+//// Memory Efficient Method \\\\
+
+let twoSum = function (nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let value = nums[i];
+    let pair = target - value;
+    if (map[pair] !== undefined) {
+      return [map[pair], i];
+    } else {
+      map[value] = i;
+    }
+  }
+};
+
+//// Solution \\\\
+
+let twoSum = function (nums, target) {
+  let map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let value = nums[i];
+    let pair = target - value;
+    if (map[pair] !== undefined) {
+      return [map[pair], i];
+    } else {
+      map[value] = i;
+    }
+  }
+};
+
+let myArr = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 131, 89, 90, 576, 167, 100, 875, 12, 14, 1111, 78,
+  90, 745, 230, 64, 78, 90, 789, 90, 90, 68, 90, 9999, 234452, 245, 245, 67, 34,
+  356, 67,
+];
+
+console.log(twoSum(myArr, 13));
