83a5b06168f27d3f1c7624623ce86c7e8d3a55a7
Blain Crawford <blaincrawford@Blains-MacBook-Pro.local>
2021-12-07 12:59:12 -0500

    updating README with info on palindrome, and solution in palindrome doc

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 Practicing Leet Code Algorithms
 
 Table of Contents: 
-1. TwoSum 
+1. twoSum 
     - Examples for inputs and expected outputs
     - Brute Force Method
     - Memory Efficient Method
     - Solution
-2. Second item
+2. isPalindrome
+    - Inputs, Ouputs, and Constraints
+    - Brute Force Method
+    - Memory Efficient method
+    - Solution 
 3. Third item
 4. Fourth item
-    
---
+++ isPalindrome.js
@@ -0,0 +1,72 @@
+// Given an integer x, return true if x is palindrome integer.
+
+// An integer is a palindrome when it reads the same backward as forward.
+
+// For example, 121 is a palindrome while 123 is not.
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {};
+
+//  Example 1:
+
+// Input: x = 121
+// Output: true
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+// Example 4:
+
+// Input: x = -101
+// Output: false
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+
+//// Brute Force Solution \\\\
+var isPalindrome = function (x) {
+  let itIsPalindrome = true;
+  let forwardArr = x.toString().split("");
+  let backwardArr = x.toString().split("");
+  backwardArr.reverse();
+
+  for (let i = 0; i < forwardArr.length; i++) {
+    if (parseInt(forwardArr[i]) === parseInt(backwardArr[i])) {
+      itIsPalindrome = true;
+    } else {
+      itIsPalindrome = false;
+      return itIsPalindrome;
+      n;
+    }
+  }
+  return itIsPalindrome;
+};
+
+//// Memory Efficient Method \\\\
+
+var isPalindrome = function (x) {
+  if (x < 0) {
+    return false;
+  }
+
+  let reverse = 0;
+
+  for (let i = x; i >= 1; i = Math.floor(i / 10)) {
+    reverse = reverse * 10 + (i % 10);
+  }
+  return reverse === x;
+};
+
+
+console.log(isPalindrome(1234321));
+console.log(isPalindrome(12345321));
