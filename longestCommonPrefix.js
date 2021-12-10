0de117675538bef133aff7c0e2ec0669e184a24b
Blain Crawford <blaincrawford@gmail.com>
2021-12-10 10:58:12 -0500

    getting first answer in for longestCommonPrefix

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -19,6 +19,16 @@
  * @return {string}
  */
  var longestCommonPrefix = function(strs) {
-
+    if(strs.length === 0 || strs === null){
+        return "";
+    }
     
+    let prefix = strs[0];
+    
+    for(let i = 1; i < strs.length; i++){
+        while(strs[i].indexOf(prefix) !== 0){
+            prefix = prefix.substring(0, prefix.length - 1)
+        }
+    }
+    return prefix;
 };
