39cefcac132a7f8277a5aaef78f6ce7840966a2f
Blain Crawford <blaincrawford@gmail.com>
2021-12-16 10:52:19 -0500

    couldn't figure out today, but will try again tomorrow

---
+++ isValid.js
@@ -0,0 +1,39 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var isValid = function (s) {
+  //set boolean true
+  //iterate through string
+    //set string at i to character
+    //set string at i plus 1 to following character
+    //if character = ( or [ or { and next character doesnt equal ) or ] or }
+      //change bolean to false and return boolean
+  //return boolean
+};
+
+parenthesisExample1 = '{}'
+isValid(parenthesisExample1)
+parenthesisExample2 = '{]'
+isValid(parenthesisExamples)
