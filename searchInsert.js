8e80d6d0583e1f47820cb9919e59ecaf9c05906e
Blain Crawford <blaincrawford@gmail.com>
2022-01-27 09:57:44 -0500

    solved searchInsert

--- searchInsert.js
+++ searchInsert.js
@@ -31,5 +31,10 @@
  * @return {number}
  */
  var searchInsert = function(nums, target) {
-    
+  for(let i = 0; i < nums.length; i++){
+      if(nums[i] === target || nums[i] > target){
+          return i;
+      } 
+  }
+  return nums.length;
 };
